fc632afc451749f5e7aa49ba78d1aca198cc6b28
Nithin <nithin.cs@litmus7.com>
2025-03-03 14:18:20 +0530

    questions and Topic slides

---
+++ coding-practise/javascript/questions.js
@@ -0,0 +1,11 @@
+// 1. Write some code which explain diff. b/w let and const
+// 2. write a function which accepts two arguments, to add two numbers and return the result
+//      2.1 convert the above method into an arrow function
+// 3. Callback example: Pass a function as argument to above function, and that should get executed inside it.
+//     printResult(param) { console.log('The result is' + param) }
+// 4. Values of this in different situations
+// 5. Closure : Write a function that accepts a number(multiplier) and this function should return another function,
+//      that should have the capability to accept a number and return a result which should be multiplied with the earlier multiplier
+//  multiplyByX(10) => multiplyBy10 => multiplyBy10(100)
+// 6. Promise : 
+//      const myHeavyTask = () => new Promise((resolve, reject) => setTimeout(()=> resolve('Hello world!'), 8000))
---
+++ coding-practise/javascript/sample.js
@@ -0,0 +1,2 @@
+// 2. write a function which accepts two arguments, to add two numbers and return result
+const add = (a, b) => a + b;
